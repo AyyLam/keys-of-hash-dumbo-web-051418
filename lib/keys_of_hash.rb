6f57b8d0c6c801cdaa77617f96e93b7de9cf4864
Anthony Lam <al3101@nyu.edu>
2018-05-08 19:13:56 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(*arguments)
     res = []
     self.each do |key, value| 
-      if value == arguments 
+      if arguments.include?(value) 
         res << key
       end
     end
